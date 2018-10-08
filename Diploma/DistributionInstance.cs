f13522f6474023e8b4333d08456398483e3cb113
Oleg <lobandml>
2018-10-08 04:56:18 +0300

    Так, начало положено - я потратил пару часов на сохранение файлов

---
+++ Diploma/DistributionInstance.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Diploma
+{
+    class DistributionInstance
+    {
+        public Dictionary<string,double> DParams;
+        public List<double> arr;
+        public DistributionInstance(double[] inputarr)
+        {
+            DParams = new Dictionary<string, double>();
+            arr = inputarr.ToList();
+        }
+        public DistributionInstance()
+        {
+            DParams = new Dictionary<string, double>();
+            arr = new List<double>();
+        }
+        public XmlDocument ConvertToXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            var root = doc.CreateElement("Instance");
+            doc.AppendChild(root);
+            var list = DParams.ToList();
+            for (int i=0; i< list.Count;i++)
+            {
+                var cel = doc.CreateElement("Param");
+                cel.SetAttribute("name", list[i].Key);
+                cel.InnerText = list[i].Value.ToString(CultureInfo.InvariantCulture);
+                root.AppendChild(cel);
+            }
+            for (int i = 0; i < arr.Count; i++)
+            {
+                var cel = doc.CreateElement("Item");
+                cel.InnerText = arr[i].ToString(CultureInfo.InvariantCulture);
+                root.AppendChild(cel);
+            }
+            return doc;
+        }
+        public void InitXml(string doc)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.LoadXml(doc);
+            InitXml(xdoc);
+        }
+        public void InitXml(XmlDocument doc)
+        {
+            var instance = doc.SelectSingleNode("Instance");
+            var SParams = instance.SelectNodes("Param");
+            var SItems = instance.SelectNodes("Item");
+            DParams.Clear();
+            arr.Clear();
+            for (int i=0;i<SParams.Count;i++)
+            {
+                var parElement = (XmlElement)SParams[i];
+                DParams.Add(parElement.GetAttribute("name"), Double.Parse(parElement.InnerText, CultureInfo.InvariantCulture));
+            }
+            for (int i = 0; i < SParams.Count; i++)
+            {
+                var itemElement = (XmlElement)SItems[i];
+                arr.Add(Double.Parse(itemElement.InnerText, CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
--- Diploma/GammaGeneration.xaml.cs
+++ Diploma/GammaGeneration.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,8 @@ namespace Diploma
 	/// </summary>
 	public partial class GammaGeneration : Window
 	{
+		double[] currGammaArray;
+        DistributionInstance currentDis;
 		public GammaGeneration()
 		{
 			InitializeComponent();
@@ -28,5 +32,63 @@ namespace Diploma
 		{
 
 		}
-	}
+
+		private void button_Click(object sender, RoutedEventArgs e)
+		{
+			double theta_param = double.Parse(param1tb.Text);
+			double k = double.Parse(param2tb.Text);
+			int count = int.Parse(counttb.Text);
+			Accord.Statistics.Distributions.Univariate.GammaDistribution gd1 = new Accord.Statistics.Distributions.Univariate.GammaDistribution(theta_param, k);
+			double[] dd1 = gd1.Generate(count);
+			currGammaArray = dd1;
+			UpdateList(currGammaArray);
+            DistributionInstance dis = new DistributionInstance(currGammaArray);
+            dis.DParams.Add("theta", theta_param);
+            dis.DParams.Add("k", k);
+            currentDis = dis;
+        }
+		private void UpdateList(double[] input)
+		{
+			listBox.Items.Clear();
+			for (int i=0;i<input.Length;i++)
+			{
+				listBox.Items.Add(input[i]);
+			}
+		}
+        private void SaveGenerated()
+        {  
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "XML Document|*.xml";
+            saveFileDialog1.Title = "Save an XML Document";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                currentDis.ConvertToXml().Save(saveFileDialog1.FileName);
+            }
+        }
+        private void OpenGenerated()
+        {
+            OpenFileDialog ofd1 = new OpenFileDialog();
+            ofd1.Filter = "XML Document|*.xml";
+            ofd1.Title = "Open an XML Document";
+            ofd1.ShowDialog();
+            if (ofd1.FileName != "")
+            {
+                currentDis = new DistributionInstance();
+                string str = File.ReadAllText(ofd1.FileName);
+                currentDis.InitXml(str);
+            }
+        }
+
+        private void SaveGenButtonHandler(object sender, RoutedEventArgs e)
+        {
+            SaveGenerated();
+        }
+
+        private void OpenGenButtonHandler(object sender, RoutedEventArgs e)
+        {
+            OpenGenerated();
+        }
+    }
 }
