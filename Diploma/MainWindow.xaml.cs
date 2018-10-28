39213541acf5282d4efb6ba512c2750a0c3daa34
Oleg <lobandml>
2018-10-28 15:43:33 +0200

    +++

--- Diploma/MainWindow.xaml.cs
+++ Diploma/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 using Accord;
 using Microsoft.Win32;
 using System.IO;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
 
 namespace Diploma
 {
@@ -24,12 +27,16 @@ namespace Diploma
 	public partial class MainWindow : Window
 	{
         DistributionInstance currentDis;
+        // sequental, для посл. анализа
+        public SeriesCollection SCollection { get; set; }
         public MainWindow()
 		{
 			InitializeComponent();
-			Accord.Statistics.Distributions.Univariate.GammaDistribution gd1 = new Accord.Statistics.Distributions.Univariate.GammaDistribution(1,1);
-			double dd1 = gd1.Generate();
-		}
+			//Accord.Statistics.Distributions.Univariate.GammaDistribution gd1 = new Accord.Statistics.Distributions.Univariate.GammaDistribution(1,1);
+			//double dd1 = gd1.Generate();
+            var mapper = Mappers.Xy<System.Windows.Point>().X(v => v.X).Y(v => v.Y);
+            SCollection = new SeriesCollection(mapper);
+        }
 
         private void OpenGeneratorHandler(object sender, RoutedEventArgs e)
         {
@@ -50,31 +57,176 @@ namespace Diploma
                 string str = File.ReadAllText(ofd1.FileName);
                 currentDis.InitXml(str);
             }
+            // test1();
+            //test2theta();
+        }
+
+        private void test2theta()
+        {
+            double alpha = 0.1;
+            double beta = 0.01;
+            double c0;
+            double c1; 
+
+
+            while (SCollection.Count > 0)
+            {
+                SCollection.RemoveAt(SCollection.Count - 1);
+            }
+
+            var topPointsLine = AddNewSeriesCollection("Top", Brushes.Red);
+            var botPointsLine = AddNewSeriesCollection("Bot", Brushes.Blue);
+            var checkingsPointsLine = AddNewSeriesCollection("values", Brushes.Aqua);
+            DataContext = this;
+
+            for (int i = 0; i < currentDis.arr.Count; i++)
+            {
+                List<double> checkedList = currentDis.arr.GetRange(0, i + 1);
+                double likehoodDivision = SPRT.LikehoodKnownTheta(checkedList);
+                //c0 = SPRT.LowMarginKnownTheta(currentDis.DParams["k"] + 0.9, currentDis.DParams["k"], currentDis.DParams["theta"], alpha, beta, i + 1);
+                //c1 = SPRT.TopMarginKnownTheta(currentDis.DParams["k"] + 0.9, currentDis.DParams["k"], currentDis.DParams["theta"], alpha, beta, i + 1);
+                c0 = SPRT.LowMarginKnownTheta(double.Parse(textBox.Text, System.Globalization.CultureInfo.InvariantCulture), double.Parse(textBox_Copy.Text, System.Globalization.CultureInfo.InvariantCulture), currentDis.DParams["theta"], alpha, beta, i + 1);
+                c1 = SPRT.TopMarginKnownTheta(double.Parse(textBox.Text, System.Globalization.CultureInfo.InvariantCulture), double.Parse(textBox_Copy.Text, System.Globalization.CultureInfo.InvariantCulture), currentDis.DParams["theta"], alpha, beta, i + 1);
+
+                topPointsLine.Values.Add(new System.Windows.Point(i, Math.Round(c1,3)));
+                botPointsLine.Values.Add(new System.Windows.Point(i, Math.Round(c0, 3)));
+                checkingsPointsLine.Values.Add(new System.Windows.Point(i, Math.Round(likehoodDivision, 3)));
+
+                if (likehoodDivision < c0)
+                {
+                    //MessageBox.Show("h0 - " + i.ToString());
+                    //break;
+                }
+
+                if (likehoodDivision > c1)
+                {
+                    //MessageBox.Show("h1 - " + i.ToString());
+                    //break;
+                }
+            }
+        }
+        private void test2K()
+        {
+            double alpha = 0.1;
+            double beta = 0.05;
+            double c0;
+            double c1;
+
+
+            while (SCollection.Count > 0)
+            {
+                SCollection.RemoveAt(SCollection.Count - 1);
+                
+            }
+
+            var topPointsLine = AddNewSeriesCollection("брак.значение", Brushes.Red);
+            var botPointsLine = AddNewSeriesCollection("пр-е значение", Brushes.Blue);
+            var checkingsPointsLine = AddNewSeriesCollection("наблюдения", Brushes.Purple);
+            DataContext = this;
+
+            for (int i = 0; i < currentDis.arr.Count; i++)
+            {
+                List<double> checkedList = currentDis.arr.GetRange(0, i + 1);
+                double likehoodDivision = SPRT.LikehoodKnownK(checkedList);
+                //c0 = SPRT.LowMarginKnownTheta(currentDis.DParams["k"] + 0.9, currentDis.DParams["k"], currentDis.DParams["theta"], alpha, beta, i + 1);
+                //c1 = SPRT.TopMarginKnownTheta(currentDis.DParams["k"] + 0.9, currentDis.DParams["k"], currentDis.DParams["theta"], alpha, beta, i + 1);
+                c0 = SPRT.LowMarginKnownK(double.Parse(textBox.Text, System.Globalization.CultureInfo.InvariantCulture), double.Parse(textBox_Copy.Text, System.Globalization.CultureInfo.InvariantCulture), currentDis.DParams["k"], alpha, beta, i + 1);
+                c1 = SPRT.TopMarginKnownK(double.Parse(textBox.Text, System.Globalization.CultureInfo.InvariantCulture), double.Parse(textBox_Copy.Text, System.Globalization.CultureInfo.InvariantCulture), currentDis.DParams["k"], alpha, beta, i + 1);
+                //if (c0 > c1)
+               // {
+                 //   var tt = c0;
+                //    c0 = c1;
+               //     c1 = tt;
+                //}
+
+                topPointsLine.Values.Add(new System.Windows.Point(i, Math.Round(c1, 3)));
+                botPointsLine.Values.Add(new System.Windows.Point(i, Math.Round(c0, 3)));
+                checkingsPointsLine.Values.Add(new System.Windows.Point(i, Math.Round(likehoodDivision, 3)));
+
+                if (likehoodDivision < c0)
+                {
+                    MessageBox.Show("h0 - " + i.ToString());
+                    break;
+                }
+
+                if (likehoodDivision > c1)
+                {
+                    MessageBox.Show("h1 - " + i.ToString());
+                    break;
+                }
+            }
+        }
+
+        private void test1()
+        {
             double alpha = 0.1;
             double beta = 0.01;
             double c0 = beta / (1 - alpha);
             c0 = Math.Log(c0);
             double c1 = (1 - beta) / alpha;
             c1 = Math.Log(c1);
+
+
             for (int i = 0; i < currentDis.arr.Count; i++)
             {
                 List<double> checkedList = currentDis.arr.GetRange(0, i + 1);
-                double checkedForThe0 = SPRT.GammaLikelihood(checkedList, currentDis.DParams["k"] + 0.4, currentDis.DParams["theta"]);
+                double checkedForThe0 = SPRT.GammaLikelihood(checkedList, currentDis.DParams["k"]+0.9, currentDis.DParams["theta"]);
                 double checkedForThe1 = SPRT.GammaLikelihood(checkedList, currentDis.DParams["k"], currentDis.DParams["theta"]);
                 double likehoodDivision = checkedForThe1 / checkedForThe0;
                 likehoodDivision = Math.Log(likehoodDivision);
                 if (likehoodDivision < c0)
                 {
-                    MessageBox.Show("h0" + i.ToString());
+                    MessageBox.Show("h0 - " + i.ToString());
                     break;
                 }
 
                 if (likehoodDivision > c1)
                 {
-                    MessageBox.Show("h1" + i.ToString());
+                    MessageBox.Show("h1 - " + i.ToString());
                     break;
                 }
             }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //test1();
+            if (currentDis == null)
+            {
+                MessageBox.Show("Файл подгрузи, а?");
+                return;
+            }
+            test2theta();
+        }
+        public LineSeries AddNewSeriesCollection(string title, System.Windows.Media.Brush color)
+        {
+            var nls = new LineSeries
+            {
+                Title = title,
+                //Values = new LiveCharts.ChartValues<float>(values),
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometrySize = 5,
+                PointForeground = color,
+                Stroke = color
+            };
+            nls.Values = new ChartValues<System.Windows.Point>();
+            SCollection.Add(nls);
+            return nls;
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox_Copy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void checkK_Click(object sender, RoutedEventArgs e)
+        {
+            test2K();
         }
     }
 }
--- Diploma/SPRT.cs
+++ Diploma/SPRT.cs
@@ -17,5 +17,70 @@ namespace Diploma
             }
             return result;
         }
+
+        public static double LikehoodKnownK(List<double> input)
+        {
+            double result = 0;
+            for (int i =0;i<input.Count;i++)
+            {
+                result += input[i];
+            }
+            return result;
+        }
+        public static double TopMarginKnownK(double theta0, double theta1, double k, double alpha, double beta, double n)
+        {
+            double result = 0;
+            result = Math.Log((1 - beta) / alpha) + k * n * Math.Log(theta1) - k * n * Math.Log(theta0);
+            result = result / ((1 / theta0) - (1 / theta1));
+            return result;
+        }
+        public static double LowMarginKnownK(double theta0, double theta1, double k, double alpha, double beta, double n)
+        {
+            double result = 0;
+            result = Math.Log(beta / (1 - alpha)) + k * n * Math.Log(theta1) - k * n * Math.Log(theta0);
+            result = result / ((1 / theta0) - (1 / theta1));
+            return result;
+        }
+        public static double LikehoodKnownTheta(List<double> input)
+        {
+            double result = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                result += Math.Log(input[i]);
+            }
+            return result;
+        }
+        //public static double TopMarginKnownTheta(double k0, double k1, double theta, double alpha, double beta, double n)
+        //{
+        //    double result = 0;
+        //    result = ((1 - beta) / alpha) * Math.Pow(theta, k1) * Math.Pow(Accord.Math.Gamma.Function(k1), n) / (Math.Pow(theta, k0) * Math.Pow(Accord.Math.Gamma.Function(k0), n));
+        //    result = Math.Log(result);
+        //    result = result / (k1 - k0);
+        //    return result;
+        //}
+        //public static double LowMarginKnownTheta(double k0, double k1, double theta, double alpha, double beta, double n)
+        //{
+        //    double result = 0;
+        //    result = result = beta / (1-alpha) * Math.Pow(theta, k1) * Math.Pow(Accord.Math.Gamma.Function(k1), n) / (Math.Pow(theta, k0) / Math.Pow(Accord.Math.Gamma.Function(k0), n));
+        //    result = Math.Log(result);
+        //    result = result / (k1 - k0);
+        //    return result;
+        //}
+        public static double TopMarginKnownTheta(double k0, double k1, double theta, double alpha, double beta, double n)
+        {
+            double result = 0;
+            result = ((1 - beta) / alpha) * Math.Pow(theta, k1) * Math.Pow(Accord.Math.Gamma.Function(k1), n) / (Math.Pow(theta, k0) * Math.Pow(Accord.Math.Gamma.Function(k0), n));
+            result = Math.Log(result);
+            result = result / (k1 - k0);
+            return result;
+        }
+        public static double LowMarginKnownTheta(double k0, double k1, double theta, double alpha, double beta, double n)
+        {
+            double result = 0;
+            result = result = beta / (1 - alpha) * Math.Pow(theta, k1) * Math.Pow(Accord.Math.Gamma.Function(k1), n) / (Math.Pow(theta, k0) / Math.Pow(Accord.Math.Gamma.Function(k0), n));
+            result = Math.Log(result);
+            result = result / (k1 - k0);
+            return result;
+        }
     }
 }
